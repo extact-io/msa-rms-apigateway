728ec35dce878d9e543fb185b18499b2fcb225a8
extact-io <ozytso@gmail.com>
2023-05-09 22:30:09 +0900

    improve: dockerfile

--- env/docker/docker-entrypoint.sh
+++ env/docker/docker-entrypoint.sh
@@ -4,4 +4,4 @@ if [ -n "$LOGBACK_CONFIG_PATH" ]; then
   LOGBACK_CONFIG_OPT="-Dlogback.configurationFile="${LOGBACK_CONFIG_PATH}
 fi
 
-java ${LOGBACK_CONFIG_OPT} -jar $1
+exec java ${LOGBACK_CONFIG_OPT} -jar $1
